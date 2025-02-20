da9933425d74704522021340345b0012cef00a87
katajisc <katajisc@arcada.fi>
2025-02-20 11:32:06 +0200

    initial commit
    Uploaded the folder "backend-startpunkt2" w/ included files.

---
+++ backend-startpunkt2/handy_methods.php
@@ -0,0 +1,21 @@
+<?php
+// Start the session
+session_start();
+// Allt möjligt viktigt som vi använder ofta, sessionshantering, form validation etc.
+
+// En funktion som tar bort whitespace, backslashes (escape char) och gör om < till html safe motsvarigheter
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+// Databaskonfiguration
+$servername = "localhost";
+$dbname = "bistromd";
+$username = "bistromd";
+include "hemlis.php";
+
+
+?>
